bd3beeed05f6a8d85562d2a3601023b4e58eba98
Bosburp <bosburp@gmail.com>
2026-09-08 17:11:42 +0200

    Extract frontend assets from HTML shell

--- scripts/check-inline-script.mjs
+++ scripts/check-inline-script.mjs
@@ -4,6 +4,12 @@ const html = fs.readFileSync('index.html', 'utf8');
 const scripts = [...html.matchAll(/<script(?![^>]*application\/ld\+json)[^>]*>([\s\S]*?)<\/script>/g)]
   .map(match => match[1])
   .join('\n');
+const localScriptSources = [...html.matchAll(/<script\b[^>]*\bsrc=["']([^"']+)["'][^>]*><\/script>/g)]
+  .map(match => match[1])
+  .filter(src => !/^https?:\/\//i.test(src));
+const localScripts = localScriptSources
+  .map(src => fs.readFileSync(src, 'utf8'))
+  .join('\n');
 
-new Function(scripts);
-console.log('inline script syntax ok');
+new Function(`${scripts}\n${localScripts}`);
+console.log('application script syntax ok');
